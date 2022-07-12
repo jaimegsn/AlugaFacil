f66e7e4fc87f4025c3f7fa9f1bcd2dba9ff2eeb6
Jaime Neto <jaime.jgsn2@gmail.com>
2022-07-12 03:00:12 -0300

    Update settings.py

--- aluga_facil/settings.py
+++ aluga_facil/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--_)6qd5m67x!^f8dja%a5xvu%62mud@$c1un!s=xhc#*gmli%i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
